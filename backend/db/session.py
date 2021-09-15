bdc14236d3d03abd5952d32f5c6391e68fe84cc6
ravellys <ravellyspyrrho@gmail.com>
2021-09-14 23:47:12 -0300

    sqlite

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,8 +3,14 @@ from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
+# conectar com o postgres
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# conectar com sqlite
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
