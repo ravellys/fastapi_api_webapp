8c3dcc77400490b975941c8161435060e25ee606
ravellys <ravellyspyrrho@gmail.com>
2021-09-14 23:36:11 -0300

    config inicial

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
-
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
+import uvicorn
 
-app = FastAPI(title=settings.PROJECT_TITLE, varion=settings.PROJECT_VERSION)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, varion=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
 def hello_api():
     return {"message": "Hello World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
