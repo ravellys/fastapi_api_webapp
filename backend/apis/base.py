db7fd0849c5f3297737e161f13a3fc1147da2a35
ravellys <ravellyspyrrho@gmail.com>
2021-09-22 23:12:48 -0300

    create jobs

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from apis.version1 import route_users
-
+from apis.version1 import route_users, route_jobs
 
 api_router = APIRouter()
 
 api_router.include_router(route_users.router, prefix='/users', tags=["users"])
+api_router.include_router(route_jobs.router, prefix='/job', tags=["Jobs"])
